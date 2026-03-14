55eef24c5fc4a9054b6f5c26764e0e2e51f033c1
YOUR_NAME <YOUR_EMAIL>
2026-03-14 14:34:55 +0300

    T003 has completed

---
+++ tests/test_config.py
@@ -0,0 +1,27 @@
+import pytest
+from pydantic import ValidationError
+from app.config import SimpleConfig
+
+def test_get_settings_success(monkeypatch):
+    # Этап 1: Подменяем переменные окружения (monkeypatch.setenv)
+    # Нам нужно замокать ВСЕ обязательные поля, чтобы класс собрался.
+    # ВАЖНО: monkeypatch работает со строками, поэтому порт передаем как "8080"
+    monkeypatch.setenv("DATABASE_URL", "postgres://test_user:test_pass@localhost:5432/test_db")
+    monkeypatch.setenv("APP_PORT", "8080")
+
+    # Этап 2: Инстанцируем класс конфигурации.
+    # Прямо сейчас он прочитает замоканные переменные из monkeypatch!
+    settings = SimpleConfig()
+
+    # Этап 3: Проверяем (assert), что загрузилось то, что мы передали
+    assert settings.database_url == "postgres://test_user:test_pass@localhost:5432/test_db"
+    # Pydantic сам должен был привести "8080" к числу 8080
+    assert settings.app_port == 8080
+
+def test_get_settings_fail_missing_required_url(monkeypatch):
+    # Этап 1: Специально УДАЛЯЕМ обязательную переменную из окружения
+    monkeypatch.delenv("DATABASE_URL", raising=False)
+
+    # Этап 2: Проверяем, что при попытке создать конфиг будет выброшена ошибка ValidationError
+    with pytest.raises(ValidationError):
+        SimpleConfig()
