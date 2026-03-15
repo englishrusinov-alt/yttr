820cf81ef956aeaecd9b56156923a0b11481d7ee
YOUR_NAME <YOUR_EMAIL>
2026-03-15 16:48:37 +0300

    T005 completed

---
+++ app/main.py
@@ -0,0 +1,47 @@
+import uuid
+
+import fastapi
+import structlog
+
+from contextlib import contextmanager, asynccontextmanager
+
+structlog.configure(
+    processors=[
+        structlog.contextvars.merge_contextvars,
+        structlog.processors.add_log_level,
+        structlog.processors.JSONRenderer
+    ]
+)
+logger = structlog.get_logger()
+
+
+@asynccontextmanager
+async def lifespan(app:fastapi.FastAPI):
+    logger.info("Application startup")
+    yield
+    logger.info("Application shutdown")
+
+app = fastapi.FastAPI(lifespan=lifespan)
+
+# 3. Middleware (Промежуточное ПО). Выполняется для КАЖДОГО запроса.
+@app.middleware("http")
+async def add_request_id_logging(request:fastapi.Request,call_next):
+    # генерируем уникальный id
+    req_id = str(uuid.uuid4())
+
+    structlog.contextvars.clear_contextvars()
+    structlog.contextvars.bind_contextvars(request_id = req_id)
+
+    logger.info("request_started",path = request.url.path,method = request.method)
+    # Передаем запрос дальше (в роутер)
+    response = await call_next(request)
+    # Заставляем FastAPI вернуть этот же ID клиенту в заголовках (хорошая практика)
+    response.headers["X-Request-Id"] = req_id
+    # Засекаем, что запрос завершен
+    logger.info("request_finished", status=response.status_code)
+    return response
+
+@app.get("/dashboard")
+def dashboard():
+    logger.info("Dashboard called")
+    return {"status":"ok"}
